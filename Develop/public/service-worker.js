0eb81f697992949508b610040cb85676d1ce4dd1
e-leal <leal.elyssa03@gmail.com>
2020-09-27 21:39:05 -0500

    corrected typo in install listener and added start url to manifest

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -2,18 +2,20 @@ const CACHE_NAME = 'budget-tracker-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
-    './index.html',
-    './css/styles.css',
-    './icons/icon-72x72.png',
-    './icons/icon-96x96.png',
-    './icons/icon-128x128.png',
-    './icons/icon-144x144.png',
-    './icons/icon-152x152.png',
-    './icons/icon-192x192.png',
-    './icons/icon-384x384.png',
-    './icons/icon-512x512.png',
-    './js/index.js',
-    './manifest.json'
+    '/',
+    '/index.html',
+    '/css/styles.css',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png',
+    '/js/index.js',
+    '/manifest.json',
+
 ]
 
 self.addEventListener('install', function (e){
@@ -23,4 +25,59 @@ self.addEventListener('install', function (e){
             return cache.addAll(FILES_TO_CACHE)
         })
     )
+})
+
+self.addEventListener('activate', function (e){
+    e.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if(key !== CACHE_NAME && key !== DATA_CACHE_NAME){
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    )
+})
+
+self.addEventListener('fetch', function (e){
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              return fetch(e.request)
+                .then(response => {
+                  // If the response was good, clone it and store it in the cache.
+                  if (response.status === 200) {
+                    cache.put(e.request.url, response.clone());
+                  }
+    
+                  return response;
+                })
+                .catch(err => {
+                  // Network request failed, try to get it from the cache.
+                  return cache.match(e.request);
+                });
+            })
+            .catch(err => console.log(err))
+        );
+    
+        return;
+      }
+    
+      e.respondWith(
+        fetch(e.request).catch(function() {
+          return caches.match(e.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (e.request.headers.get('accept').includes('text/html')) {
+              // return the cached home page for all requests for html pages
+              return caches.match('/');
+            }
+          });
+        })
+      );
 })
